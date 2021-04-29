7ffb7266fb68b82bdebc7db8f6e455dea28156be
nombre <daniel.b100726@gmail.com>
2021-04-28 20:23:49 -0400

    clase 11 obtener personajes desde la API

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,17 @@
-const home = () => {
+import getData from "../utils/getData"
+const home = async () => {
+    const characters = await getData()
     const view = `
-    <div class="characters">
-        <article class="character-item">
-            <a href="#/1/">
-                <img src="image" alt="name" tittle="name">
-                <h2>Name</h2>
-            </a>
-        </article>
-    </div>
+        <div class="characters">
+            ${character.results.map(character => `
+                <article class="character-item">
+                <a href="#/${character.id}/">
+                    <img src="${character.image}" alt="${character.name}" tittle="${character.name}">
+                    <h2>${character.name}</h2>
+                </a>
+                </article>
+            `).join("")}
+        </div>
     `
     return view
 }
